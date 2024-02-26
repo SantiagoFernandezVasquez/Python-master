1121b8535f356c2412d2798db6e02a8f70b9b6ca
SantiagoFernandezVasquez <160640951+SantiagoFernandezVasquez@users.noreply.github.com>
2024-02-26 13:13:11 -0500

    Algoritmo para hacer una tabla de multiplicar

---
+++ Ejercicio9.py
@@ -0,0 +1,10 @@
+Num1 = int(input("Ingresa el numero que desea multiplicar: "))
+
+for i in range(1, 11):
+    if Num1 ==0:
+        print("El resultado es 0")      
+    else:
+        tablamultiplicar= Num1*i
+        print("El resultado es", tablamultiplicar)
+        
+    
